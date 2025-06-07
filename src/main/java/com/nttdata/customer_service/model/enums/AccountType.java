fde3f01492d45ad91e0ae5bb052084c807677409
Carlos Valverde <cvalverdecarreno@gmail.com>
2025-06-07 12:36:46 -0500

    Nuevos cambios

--- src/main/java/com/nttdata/customer_service/model/enums/AccountType.java
+++ src/main/java/com/nttdata/customer_service/model/enums/AccountType.java
@@ -1,7 +1,7 @@
 package com.nttdata.customer_service.model.enums;
 
 public enum AccountType {
-    CHECKING,       // Cuenta corriente
-    SAVINGS,        // Cuenta de ahorros
-    FIXED_TERM      // Plazo fijo
+    CHECKING,
+    SAVINGS,
+    FIXED_TERM
 }
